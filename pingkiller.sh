1d59e38d3156c85a4be7ef2aa591fc48c82f79e9
umbraexy <101243382+umbraexy@users.noreply.github.com>
2022-03-16 06:27:49 -0400

    Update pingkiller.sh

--- pingkiller.sh
+++ pingkiller.sh
@@ -4,7 +4,7 @@
 # redirects information extracted from ps
 ps > ps.txt
 # extracts PID from process line
-cat ps.txt |grep "ping" |cut -d " " -f 4  > pid.txt
+cat ps.txt |grep "ping" |cut -d " " -f 3  > pid.txt
 # kills the ping process by using the extracted PID
 for pid in $(cat pid.txt); do kill $pid 2> /dev/null  &
  done
